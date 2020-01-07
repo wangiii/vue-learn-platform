3c090c64a825364cc983a7e7756c57d44675a484
JasonVV <jasonwangiii@outlook.com>
2020-01-07 21:52:34 +0800

    配置 babel 引入 ant-design-vue

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import { Button, Menu, Layout } from "ant-design-vue";
 
 Vue.config.productionTip = false;
+
+Vue.use(Button);
+Vue.use(Menu);
+Vue.use(Layout);
 
 new Vue({
   router,
